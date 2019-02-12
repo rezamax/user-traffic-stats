3586ae620695295d6e5862df29ad51ffa520074c
reza hoseeinzadeh <reza.max555@gmail.com>
2019-02-12 16:39:22 +0330

    create skeleton

---
+++ src/UtsClass.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc_mx
+ * Date: 12/02/2019
+ * Time: 04:25 PM
+ */
+
+namespace Uts;
+
+class UtsClass
+{
+
+    /**
+     * UtsClass constructor.
+     */
+    public function __construct()
+   {
+
+   }
+
+}
---
+++ tests/UnitTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc_mx
+ * Date: 12/02/2019
+ * Time: 04:35 PM
+ */
+
+namespace Uts\Test;
+
+
+class UnitTest
+{
+
+}
