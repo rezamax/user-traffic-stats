f89f28c7722631f38ceef752cc9677a946f9d89d
reza hoseeinzadeh <reza.max555@gmail.com>
2019-04-25 00:29:01 +0430

    add main class

---
+++ src/UserDayStats.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc_mx
+ * Date: 25/04/2019
+ * Time: 12:26 AM
+ */
+
+namespace Uts;
+
+
+class UserDayStats
+{
+
+}
---
+++ src/UserTotalStats.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc_mx
+ * Date: 25/04/2019
+ * Time: 12:28 AM
+ */
+
+namespace Uts;
+
+
+class UserTotalStats
+{
+
+}
