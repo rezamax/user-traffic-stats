66328fb1efebf80bfff97f0890b5054701897162
reza hoseeinzadeh <reza.max555@gmail.com>
2019-04-29 23:45:38 +0430

    add userDayStats functions

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -5,11 +5,38 @@
  * Date: 12/02/2019
  * Time: 04:35 PM
  */
-
 namespace Uts\Test;
+require '..\vendor\autoload.php';
+use PHPUnit\Framework\TestCase;
+use Uts\UserDayStats;
 
-
-class UnitTest
+class UnitTest extends TestCase
 {
+    public function testCanSetUserId()
+    {
+        $user_day = new UserDayStats();
+        $id = 1;
+        $user_day->setUserId($id);
+        $this->assertEquals($id,$user_day->getUserId());
+    }
+
+    public function testFindUserById()
+    {
+        $user_day = new UserDayStats();
+        $id = 1;
+        $user_day->setUserId($id);
+        $users = $user_day->findUser();
+        if(count($users)>0) {
+            foreach ($users as $user) {
+                if ($user->id == $id) {
+                    $result = True;
+                }
+            }
+        }else{
+            $result = False;
+        }
+        $this->assertTrue($result);
+    }
+
 
 }
